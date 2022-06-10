4b7bd4800f4064548b684ab6b8e8f07b9948c51d
Rajendra <raja.iland@gmail.com>
2022-06-10 16:34:16 +0530

    controller class modified

---
+++ src/main/java/com/example/devops/rest/MyRestController.java
@@ -0,0 +1,15 @@
+package com.example.devops.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/springApp")
+public class MyRestController {
+	
+	@RequestMapping("/hi")
+	public String sayHello() {
+		return " Welcome To Devops Practice.......";
+	}
+
+}
